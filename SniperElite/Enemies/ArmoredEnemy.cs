1a3cb4fb2ac2b53b96eb35c12add0002ccd8e025
diogosequeira <diogo.sequeira@tekever.com>
2019-08-26 17:41:27 +0100

    armor

--- SniperElite/Enemies/ArmoredEnemy.cs
+++ SniperElite/Enemies/ArmoredEnemy.cs
@@ -1,9 +1,26 @@
-﻿namespace SniperElite
+﻿using System;
+
+namespace SniperElite
 {
-    public class ArmoredEnemy
+    public class ArmoredEnemy : Enemies
     {
 
         private int Armor = 100;
+        private Boolean hasArmor;
+
+
+        public Boolean getHasArmor()
+        {
+            return hasArmor;
+        }
+
+        public void setHasArmor(Boolean has)
+        {
+            hasArmor = has;
+        }
+
+
+
 
     }
 }
